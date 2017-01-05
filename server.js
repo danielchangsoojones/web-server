68120c31d1d590dff8da0dea117495364753b637
Daniel Jones <messyjones@gmail.com>
2017-01-05 09:59:00 -0500

    Initialize repo

---
+++ server.js
@@ -0,0 +1,25 @@
+var express = require("express");
+var app = express();
+
+var middleware = {
+	requireAuthentication: function (req, res, next) {
+		console.log("private rout hit!");
+		next();
+	},
+	logger: function(req, res, next) {
+		console.log(req.method + " " + req.originalUrl + " " + new Date().toString());
+		next();
+	}
+};
+
+app.use(middleware.logger);
+
+app.get("/about", middleware.requireAuthentication, function (req, res) {
+	res.send("about us");
+});
+
+app.use(express.static(__dirname + "/public"));
+
+app.listen(3000, function () {
+	console.log("express server started");
+});
